609a84fb9395b637191ef0a9c1b7f4e5440afbe4
LehOf <leeilacontell@gmail.com>
2023-06-07 10:01:59 -0300

    testando as variaveis

--- Aula01_Variaveis/Aula01_Variaveis/Program.cs
+++ Aula01_Variaveis/Aula01_Variaveis/Program.cs
@@ -1,2 +1,25 @@
-﻿
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*---- Variáveis ----*/
+
+            int x = 10;
+            string y = "Pedro";
+            double z = 10.5;
+            bool verificacao = true;
+            char sexo = 'm';
+
+            if (y == "Pedro")
+            {
+                Console.WriteLine("sim");
+            }
+
+
+        }
+    }
+}
